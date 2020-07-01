ed2594150c2fa7a7ffe67ece943c58af3bdd5d2b
Diva Krishna <divakrishnam@gmail.com>
2020-07-01 23:40:05 +0800

    add template; make login, verification, and profile form

--- landing/urls.py
+++ landing/urls.py
@@ -11,8 +11,12 @@ from . import views
 
 app_name='landing'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('login/', views.login, name='login'),
+    path('token/', views.token, name='token'),
+    path('profile/', views.profile, name='profile'),
+    
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -5,35 +5,43 @@ from django.views import generic
 from django.shortcuts import get_object_or_404,render
 from .models import Choice,Question
 
-class IndexView(generic.ListView):
-    template_name = 'landing/index.html'
-    context_object_name = 'latest_question_list'
-    
-    def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
-    
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'landing/detail.html'
+def login(request):
+    return render(request, 'landing/index.html')
 
-class ResultView(generic.DetailView):
-    model = Question
-    template_name = 'landing/results.html'
+def token(request):
+    return render(request, 'landing/results.html')
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'landing/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('landing:results', args=(question.id,)))
+def profile(request):
+    return render(request, 'landing/detail.html')
+# class IndexView(generic.ListView):
+#     template_name = 'landing/index.html'
+#     context_object_name = 'latest_question_list'
+    
+#     def get_queryset(self):
+#         return Question.objects.order_by('-pub_date')[:5]
+    
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'landing/detail.html'
+
+# class ResultView(generic.DetailView):
+#     model = Question
+#     template_name = 'landing/results.html'
+
+# def vote(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'landing/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('landing:results', args=(question.id,)))
