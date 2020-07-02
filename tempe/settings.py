be76285b74d6cbcbd59ecbfc1594edd472b807d6
trianggadios <trianggadio@gmail.com>
2020-07-02 13:48:30 +0700

    social namespace

--- tempe/settings.py
+++ tempe/settings.py
@@ -30,14 +30,14 @@ ALLOWED_HOSTS = ['tukangtempe.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'landing.apps.LandingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django'
+    'social_django',
+    'landing',
 ]
 
 MIDDLEWARE = [
