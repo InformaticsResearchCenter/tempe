f8b984012728c5ed08de7db1bffb7e7f41af1acf
trianggadios <trianggadio@gmail.com>
2020-07-02 13:14:10 +0700

    remove landing on INSTALLED_APPs

--- tempe/settings.py
+++ tempe/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django',
-    'landing'
+    'social_django'
 ]
 
 MIDDLEWARE = [
